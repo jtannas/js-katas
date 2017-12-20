c39326fa0094afa5097c2d9a524f0be0d53d7f18
Joel Tannas <jtannas@gmail.com>
2017-12-19 18:23:28 -0700

    revised level 1 solutions to comply with lighthouselabs specs

--- conditional-sum.js
+++ conditional-sum.js
@@ -1,29 +1,25 @@
+var getTestForCondition = function(condition) {
+  switch(condition) {
+  case 'even':
+    return function (testSubject) { return(testSubject % 2 === 0); };
+  case 'odd':
+    return function (testSubject) { return(testSubject % 2 === 1); };
+  }
+};
 
 var conditionalSum = function(values, condition) {
-  let sum = 0;
-  let test;
-  switch(condition) {
-    case 'even':
-      test = (currentValue) => currentValue % 2 === 0;
-      break;
-    case 'odd':
-      test = (currentValue) => currentValue % 2 === 1;
-      break;
-    default:
-      test = () => true;
-      break;
-  }
-  for (let currentValue of values){
+  var sum = 0;
+  var test = getTestForCondition(condition);
+
+  values.forEach(function(currentValue) {
     if (test(currentValue) === true){
       sum += currentValue;
     }
-  }
+  });
   return sum;
 };
 
-/* eslint-disable no-console */
 console.log(conditionalSum([1, 2, 3, 4, 5], 'even'));
 console.log(conditionalSum([1, 2, 3, 4, 5], 'odd'));
 console.log(conditionalSum([13, 88, 12, 44, 99], 'even'));
 console.log(conditionalSum([], 'odd'));
-/* eslint-enable no-console */
--- repeating-numbers.js
+++ repeating-numbers.js
@@ -1,15 +1,13 @@
 var repeatNumbers = function(repeatArrays) {
-  let buffer = [];
-  for (let repeatArray of repeatArrays){
-    let numberText = repeatArray[0].toString();
-    let repeatCount = repeatArray[1];
-    buffer.push(numberText.repeat(repeatCount));
-  }
+  var buffer = [];
+  repeatArrays.forEach(function(repeatArray){
+    var stringToRepeat = repeatArray[0].toString();
+    var repeatCount = repeatArray[1];
+    buffer.push(stringToRepeat.repeat(repeatCount));
+  });
   return buffer.join(', ');
 };
 
-/* eslint-disable no-console */
 console.log(repeatNumbers([[1, 10]]));
 console.log(repeatNumbers([[1, 2], [2, 3]]));
 console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
-/* eslint-enable no-console */
